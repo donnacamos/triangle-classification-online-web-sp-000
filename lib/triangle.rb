792a1af8ef96b17cd9ad0c7dd3f4ae8f4e6c8b41
Donna Amos <donnacamos.developer@gmail.com>
2019-03-26 15:00:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,4 +12,5 @@ def valid?
   sum_one_three = @triangle_sides[0] + @triangle_sides[2] 
   sum_two_three = @triangle_sides[1] + @triangle_sides[2] 
   
-  if @triangle_sides.none? {|side| side <= 0} 
+  if @triangle_sides.none? {|side| side <= 0} && 
+    (sum_one_two > @triangle_sides)
