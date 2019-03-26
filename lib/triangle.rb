c2fce08e8221be44734813eb37eed25226da82fa
Donna Amos <donnacamos.developer@gmail.com>
2019-03-26 15:52:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,8 @@ class Triangle
     def kind() 
       if (@side_1 <= 0) || (@side_2 <= 1) || (@side_3 <= 2) 
       raise TriangleError 
-      
+    elsif (@side_1 + @side_2 <= @side_3) || (@side_1 + @side_3 <= @side_2) || (@side_2 + @side_3 <= @side_1) 
+    
 
 class TriangleError < StandardError  
 
