6340a56b852de9230987081a17ac1c3c6e708a74
Donna Amos <donnacamos.developer@gmail.com>
2019-03-26 15:54:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     elsif (@side_1 + @side_2 <= @side_3) || (@side_1 + @side_3 <= @side_2) || (@side_2 + @side_3 <= @side_1) 
     raise TriangleError 
   else 
-    if (@side_)
+    if (@side_1 == @side_2) && (@side_2 == @side_3) 
 
 class TriangleError < StandardError  
 
